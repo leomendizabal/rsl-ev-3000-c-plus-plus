b208b5845f2b7e68eeb397db12cc2fb9db3170f8
Leo <l.mendizabal.cds@gmail.com>
2017-02-26 15:59:24 -0300

    valido parametros antes de empezar a realizar operaciones especificas del comando

--- main.cpp
+++ main.cpp
@@ -2,18 +2,20 @@
 #include "comandos.h"
 #include "errores.h"
 #include "lista_parametros.h"
+#include "lista_expresiones.h"
 
 int main(){
 
   string entrada, comando, parametros;
   Boolean salir = FALSE;
   ListaParametros listaParametros;
+  ListaExpresiones listaExpresiones;
   //Inicializo
   strcrear(entrada);
   strcrear(comando);
   strcrear(parametros);
   CrearLista(listaParametros);
-
+  CrearLista(listaExpresiones);
 
  do{
      //Leo la entrada
@@ -24,33 +26,35 @@ int main(){
     ParsearParametros(parametros,listaParametros);
    // MostrarParametros(listaParametros);
     if(ValidarComando(comando) == TRUE) {
-      if(streq(comando,"ayuda\0")){
-         Error errorA =  ValidarParametros(listaParametros,comando);
-         if( errorA == NO_ERR){
+      Error errorA =  ValidarParametros(listaParametros,comando);
+      if( errorA == NO_ERR){
+          if(streq(comando,"ayuda\0")){
             string p;
             strcrear(p);
             DarParametro(listaParametros,1,p);
             ComandoAyuda(p);
-         }else{
+          }else if(streq(comando,"atomica\0")){
+            string p;
+            strcrear(p);
+            DarParametro(listaParametros,1,p);
+            ComandoAtomica(listaExpresiones, p);
+          }else if(streq(comando,"noatomica\0")){
+
+          }else if(streq(comando,"respaldar\0")){
+
+          }else if(streq(comando,"recuperar\0")){
+
+          }else if(streq(comando,"letras\0")){
+
+          }else if(streq(comando,"evaluar\0")){
+
+          }else if(streq(comando,"salir\0")){
+            salir = TRUE;
+          }else if(streq(comando,"mostrar\0")){
+
+          }
+      }else{
             MostrarMensajeError(errorA);
-         }
-
-      }else if(streq(comando,"atomica\0")){
-
-      }else if(streq(comando,"noatomica\0")){
-
-      }else if(streq(comando,"respaldar\0")){
-
-      }else if(streq(comando,"recuperar\0")){
-
-      }else if(streq(comando,"letras\0")){
-
-      }else if(streq(comando,"evaluar\0")){
-
-      }else if(streq(comando,"salir\0")){
-        salir = TRUE;
-      }else if(streq(comando,"mostrar\0")){
-
       }
     }else{
       MostrarMensajeError(COMANDO_INV);
