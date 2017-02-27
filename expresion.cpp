0e2f440c0fff928949b2916aa06ab1d774f740dc
rgordano <rgordano@hotmail.com>
2017-02-27 14:41:32 -0300

    Update expresion.cpp

--- expresion.cpp
+++ expresion.cpp
@@ -27,13 +27,14 @@ void BajarExpresion(Expresion e, string nombreArch){
    FILE * f=fopen(nombreArch,"wb");
    BajarInt(e.indice);
    BajarArbolAux(e.arb,f);
-      
+   fclose(f);   
 }
 
 void LevantarExpresion(Expresion &e, string nombreArch){
     FILE * f = fopen (nombreArch, "rb");
     LevantarInt(e.indice);
     Levantar_Arbol(e.arb,f);
+    fclose(f);  
 }
 
 void MostrarExpresion(Expresion e){
