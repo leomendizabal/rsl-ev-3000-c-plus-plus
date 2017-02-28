693ad78f2b804c5f239b41ff43cc6ba72e58cba7
Leo <l.mendizabal.cds@gmail.com>
2017-02-28 11:03:22 -0300

    Main: se validan los parametros una sola vez, se inicializan las variables adentro del Do, se invoca la funcion ComandoAtomica

--- main.cpp
+++ main.cpp
@@ -13,14 +13,7 @@ int main(){
   ListaExpresiones listaExpresiones;
   ListaParametros listaParametros;
 
-  //Inicializo
-  strcrear(entrada);
-  strcrear(comando);
-  strcrear(parametros);
-
   CrearLista(listaExpresiones);
-  CrearLista(listaParametros);
-
 
  do{
     //Inicializo la entrada, el comando ingresado y los parametros cada vez que se ingresa
@@ -37,68 +30,35 @@ int main(){
     ParsearParametros(parametros,listaParametros);
    // MostrarParametros(listaParametros);
     if(ValidarComando(comando) == TRUE) {
-          if(streq(comando,"ayuda\0")){
-              Error errorA =  ValidarParametros(listaParametros,comando);
-              if( errorA == NO_ERR){
-                string p;
-                strcrear(p);
-                DarParametro(listaParametros,1,p);
-                ComandoAyuda(p);
+          Error error =  ValidarParametros(listaParametros,comando);
+          if( error == NO_ERR){
+              if(streq(comando,"ayuda\0")){
+                    string p;
+                    strcrear(p);
+                    DarParametro(listaParametros,1,p);
+                    ComandoAyuda(p);
+              }else if(streq(comando,"atomica\0")){
+                    string p;
+                    DarParametro(listaParametros,1,p);
+                    ComandoAtomica(listaExpresiones, p);
+              }else if(streq(comando,"noatomica\0")){
 
-             }else{
-                MostrarMensajeError(errorA);
+              }else if(streq(comando,"respaldar\0")){
+
+              }else if(streq(comando,"recuperar\0")){
+
+              }else if(streq(comando,"letras\0")){
+
+              }else if(streq(comando,"evaluar\0")){
+
+              }else if(streq(comando,"salir\0")){
+                    salir = TRUE;
+              }else if(streq(comando,"mostrar\0")){
+
+              }
+          }else{
+                MostrarMensajeError(error);
             }
-          }else if(streq(comando,"atomica\0")){
-             Error errorAt =  ValidarParametros(listaParametros,comando);
-             if(errorAt == NO_ERR){
-                string p;
-                DarParametro(listaParametros,1,p);
-                Componente compAto;
-
-                int posAto = 0;
-                if(EsVacia(listaExpresiones) == FALSE){
-                  posAto = UltimoIndice(listaExpresiones);
-                }
-                CargarDato(compAto,posAto,p[0],LETRA);
-                ArbolComponentes arbAto;
-                CrearVacio(arbAto);
-                CrearArbol(arbAto,NULL,NULL,compAto);
-                Expresion expAto;
-                posAto++;
-                CrearExpresion(expAto,arbAto,posAto);
-                InsUltimo(listaExpresiones,expAto);
-             }else{
-                MostrarMensajeError(errorAt);
-             }
-
-          }else if(streq(comando,"noatomica\0")){
-            Error errorNoAt =  ValidarParametros(listaParametros,comando);
-             if(errorNoAt == NO_ERR){
-                //Implementacion del comando
-
-             }else{
-                 MostrarMensajeError(errorNoAt);
-             }
-
-          }else if(streq(comando,"respaldar\0")){
-            Error errorResp=  ValidarParametros(listaParametros,comando);
-             if(errorResp == NO_ERR){
-                //Implementacion del comando
-
-             }else{
-                 MostrarMensajeError(errorResp);
-             }
-          }else if(streq(comando,"recuperar\0")){
-
-          }else if(streq(comando,"letras\0")){
-
-          }else if(streq(comando,"evaluar\0")){
-
-          }else if(streq(comando,"salir\0")){
-                salir = TRUE;
-          }else if(streq(comando,"mostrar\0")){
-
-          }
     }else{
       MostrarMensajeError(COMANDO_INV);
     }
